2589fd8412cdc9d6abd2686f05bd77ce23e38c87
Marcos Velasco <devslife7@gmail.com>
2020-11-20 20:18:45 -0500

    added images

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import "./App.css"
 import Contact from "./components/Contact"
 import Header from "./components/Header"
 import Projects from "./components/Projects"
+// import background1 from "./images/background1.jpg"
 
 function App() {
   return (
     <Grid container direction='column' className='App'>
       <Grid item style={{ backgroundColor: "#f4f4f4", height: "40rem" }}>
+        {/* <img src={require("./images/background1.jpg")} alt='background' /> */}
+        <img src={require("./images/svg-1.svg")} alt='background' />
+        {/* <img src={background1} alt='image' /> */}
         <Header />
       </Grid>
       <Grid item style={{ height: "40rem" }}>
