76ad9274465663560bc01484ee38ab33de82acd8
Marcos Velasco <devslife7@gmail.com>
2020-11-22 22:21:25 -0500

    adding InfoSection Elements

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/jeep.svg"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+}
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/teamwork.svg"),
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+}
+
+export const homeObjThree = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "business services",
+  headline: "Solutions that meet your needs",
+  description:
+    "We create custom and responsive web applications to saves you time when it comes to managing you business",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/teamwork.svg"),
+  alt: "Car",
+  dark: false,
+  primary: false,
+  darkText: true,
+}
