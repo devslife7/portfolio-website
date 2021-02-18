1b59ac65f4b9ab5284c480d3e9c4d74ca4e15dfa
Marcos Velasco <devslife7@gmail.com>
2021-02-18 12:41:11 -0500

    improved the design in sidebra

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -11,19 +11,19 @@ export default function Sidebar({ isOpen, toggle }) {
       </div>
         <ul className='sidebar__menu'>
           <Link className='sidebar__link' to='home' onClick={toggle} smooth='true' duration={500} spy={true} exact='true' offset={-80}>
-            H<span className="sidebar__span">o</span>me
+            H<span className="sidebar__span">om</span>e
           </Link>
           <Link className='sidebar__link' to='skills' onClick={toggle} smooth='true' duration={500} spy={true} exact='true' offset={-80}>
-            S<span className="sidebar__span">k</span>ills
+            Ski<span className="sidebar__span">ll</span>s
           </Link>
           <Link className='sidebar__link' to='projects' onClick={toggle} smooth='true' duration={500} spy={true} exact='true' offset={-80}>
-            P<span className="sidebar__span">r</span>ojects
+            P<span className="sidebar__span">ro</span>jects
           </Link>
           <Link className='sidebar__link' to='about' onClick={toggle} smooth='true' duration={500} spy={true} exact='true' offset={-80}>
-            A<span className="sidebar__span">b</span>out
+            Ab<span className="sidebar__span">ou</span>t
           </Link>
           <Link className='sidebar__link' to='contact' onClick={toggle} smooth='true' duration={500} spy={true} exact='true' offset={-80}>
-            C<span className="sidebar__span">o</span>ntact
+            C<span className="sidebar__span">on</span>tact
           </Link>
         </ul>
     </aside>
