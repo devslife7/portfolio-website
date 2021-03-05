0f3ed6b2c0d778fc90c957e2dbbe83bf11585580
Marcos Velasco <devslife7@gmail.com>
2021-03-04 21:14:08 -0500

    installed prettier

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,7 +1,7 @@
-import React from "react"
-import Button from "../Button"
-import Tag from "./Tag"
-import './Projects.css'
+import React from "react";
+import Button from "../Button";
+import Tag from "./Tag";
+import "./Projects.css";
 
 export default function Projects({
   name,
@@ -11,34 +11,39 @@ export default function Projects({
   projectLink,
   projectGif,
   technologies,
-  hosting
+  hosting,
 }) {
-
   const renderTags = (tags = []) => {
-    return tags.map( (tag, idx) =>  <Tag key={idx}>{tag}</Tag>)
-  }
+    return tags.map((tag, idx) => <Tag key={idx}>{tag}</Tag>);
+  };
 
   return (
     <>
-      <div className={`${darkTheme && 'darkBackground'}`}>
-        <div className={`project__wrapper container ${flipped && 'flipped'}`}>
+      <div className={`${darkTheme && "darkBackground"}`}>
+        <div className={`project__wrapper container ${flipped && "flipped"}`}>
           <div className="project__info">
             <p className="project__name">{name}</p>
 
             <p className="project__subtitle">{description}</p>
             <div className="project__title">Technologies:</div>
-            <div className="project__technologies">{renderTags(technologies)}</div>
+            <div className="project__technologies">
+              {renderTags(technologies)}
+            </div>
             <div className="project__title">Hosted via:</div>
             <div className="project__technologies">{renderTags(hosting)}</div>
 
             <Button href={projectLink}> Live Demo </Button>
-          </div >
-            
-          <div>
-            <img className="project__img" src={projectGif} alt={`${name} preview`} />
           </div>
-        </div >
+
+          <div>
+            <img
+              className="project__img"
+              src={projectGif}
+              alt={`${name} preview`}
+            />
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
