d1af20dd108c0684931840ea4665a3e00f89f120
Marcos Velasco <devslife7@gmail.com>
2023-06-08 15:10:07 -0400

    revert back the Heroku proxy server

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -66,28 +66,28 @@ export default function Contact() {
     clearForm()
   }
 
-  const verifyHuman = async () => {
-    // const serverURL = process.env.REACT_APP_SERVER_URL
-    const token = captchaRef.current.getValue()
-
-    // const resp = await axios.post(serverURL, { token })
-
-    // `https://cors-anywhere.herokuapp.com/https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`
-
-    const resp = await axios.post(
-      `${process.env.REACT_APP_PROXY_SERVER}/https://www.google.com/recaptcha/api/siteverify?secret=${process.env.REACT_APP_SECRET_KEY}&response=${token}`
-    )
-
-    console.log('server response: ', resp.data)
-    return resp.data
-  }
   // const verifyHuman = async () => {
-  //   const serverURL = process.env.REACT_APP_SERVER_URL + '/post'
+  //   // const serverURL = process.env.REACT_APP_SERVER_URL
   //   const token = captchaRef.current.getValue()
-  //   const resp = await axios.post(serverURL, { token })
+
+  //   // const resp = await axios.post(serverURL, { token })
+
+  //   // `https://cors-anywhere.herokuapp.com/https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`
+
+  //   const resp = await axios.post(
+  //     `${process.env.REACT_APP_PROXY_SERVER}/https://www.google.com/recaptcha/api/siteverify?secret=${process.env.REACT_APP_SECRET_KEY}&response=${token}`
+  //   )
+
   //   console.log('server response: ', resp.data)
   //   return resp.data
   // }
+  const verifyHuman = async () => {
+    const serverURL = process.env.REACT_APP_SERVER_URL + '/post'
+    const token = captchaRef.current.getValue()
+    const resp = await axios.post(serverURL, { token })
+    console.log('server response: ', resp.data)
+    return resp.data
+  }
 
   return (
     <div className='contact'>
